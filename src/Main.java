52a9777478aefbea7d83ac6d6868550322e8ccd4
AnastasiaNadezhdina <nizh110303@mail.ru>
2026-02-09 20:49:53 +0400

    Homework 5

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,12 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задача 1");
-        int clientOS = 0;
-        switch (clientOS) {
-            case 0:
-                System.out.println("Установите версию приложения для iOS по ссылке" + "\n");
-                break;
-            case 1:
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке" + "\n");
+        } else if (clientOS == 1) {
                 System.out.println("Установите версию приложения для Android по ссылке" + "\n");
-            default:
+        } else {
                 System.out.println("Операционная система не опознана" + "\n");
         }
 
